7102e08abe80b9d66f0aece8bc7f9505b4c4f522
tzledbetter <79083054+tzledbetter@users.noreply.github.com>
2021-08-18 16:58:17 +0200

    making code more efficient
    cleaning up code and reviewing what I did since I will need to present an abstract for the fluoros conference

--- code/Re-train streamline.R
+++ code/Re-train streamline.R
@@ -7,9 +7,9 @@ source("code/PaDEL_descs_calculator.R")
 source("code/reading_excel.R")
 source("code/compound_eluent.R")
 
-#lcms data ----
+#importing PFAS calibration training data (lcms) ----
 
-filename = "data/Batch 1 Semi Quant w frag.xlsx"
+filename = "Data/Batch 1 Semi Quant w frag.xlsx"
 Orbitrap_dataset_raw = read_excel_allsheets(filename)
 Orbitrap_dataset_raw = Orbitrap_dataset_raw %>%
   na.omit(Area) %>%
@@ -24,7 +24,7 @@ Orbitrap_dataset_raw = Orbitrap_dataset_raw %>%
   ))%>%
   ungroup()
 
-#smiles----
+#importing Smiles codes for PFAS in calibration set----
 
  SMILES_data = read_delim("data/Smiles_for_Target_PFAS_semicolon.csv",
                           delim = ";",
@@ -55,8 +55,7 @@ pH.aq. = 7.0
 data = Orbitrap_dataset_raw %>%
   left_join(SMILES_data)
 
-#print(data %>% select(SMILES) %>% unique())
-
+#adding eluent data to dataset
 data = data %>%
   mutate(
     RT = as.numeric(RT),
@@ -83,13 +82,16 @@ ggplot(data = training) +
   scale_x_log10() +
   scale_y_log10()
 
+#getting slopes
+
 training = training %>%
   group_by(SMILES) %>%
   mutate(slope = linear_regression(area_IC, `Theoretical Amt`)$slope) %>%
   ungroup()
 
-####convert slopes to logIE
+#####converting slopes to logIE########
 
+#importing previous trainng set
 filename = "data/IE_training_data/190714_negative_model_logIE_data.csv"
 old_training_data = read_delim(filename,
                                delim = ";",
@@ -104,6 +106,7 @@ old_training_data_filtered = old_training_data %>%
   additive=="ammonium acetate",
   pH.aq.==7.8)
 
+#Anchor IE value
 IEPFOSvalue = old_training_data_filtered$logIE
 
 Anchor_slope = training %>% 
@@ -113,6 +116,8 @@ Anchor_slope = training %>%
 
 slopePFOS <- Anchor_slope$slope #make slopePFOS a single value
 
+#Converting all slopes to values relative to slope of PFOS,
+#converting to log form, adding anchor value
 training = training %>%
   mutate(RIE =slope/slopePFOS,
   RIE =log(RIE),
@@ -166,7 +171,7 @@ forsplit <- datarbindedit%>%
   select(name) %>%
   unique()
 
-######re-calculating and joining descriptors
+######Calculating and joining descriptors for all compounds in training set
 
 descs_recalc = datarbindedit %>%
   rename(Compound = name) %>%
@@ -195,7 +200,6 @@ datarbindedit = datarbindedit %>%
   ungroup()
 
 #cleaning data
-
 datarbindeditAA = datarbindedit %>%
   dplyr::select(-c(Compound, SMILES, name, organic_modifier,additive, instrument, source, solvent))%>%
   select_if(~sum(is.na(.))< 10,)%>%
@@ -215,7 +219,7 @@ datarbindeditclean <- datarbindeditAAA %>%
   select(name,SMILES,organic_modifier,organic_modifier_percentage,
          additive, additive_concentration_mM, instrument,source,solvent, everything())
 
-#splitting
+#splitting test and training set
 
 set.seed(123) 
 split_first <- sample.split(forsplit$name, SplitRatio = 0.8)
@@ -235,9 +239,9 @@ test <- forsplit %>%
 datarbindeditclean <- rbind(train,test) %>%
   na.omit()
 
-#Training the model
+#Training model
 set.seed(123)
-folds = groupKFold(train$name, k = 5) #k - how many times 
+folds = groupKFold(train$name, k = 5)
 fitControl <- trainControl(method = "boot", index = folds)
 
 RFR <- 
@@ -249,17 +253,17 @@ RFR <-
 saveRDS(RFR,
         "regressors/PFAS_FOREST.rds")
 
+#predicting IE values 
 datarbind_with_predicted <- datarbindeditclean %>%
   mutate( logIE_predicted = predict(RFR, newdata = datarbindeditclean))
 
 datarbind_with_predicted <- datarbind_with_predicted %>%
   arrange(instrument)
 
-
 datarbind_with_predicted$split_first = factor(datarbind_with_predicted$split_first,
                                   levels = c(TRUE, FALSE),
                                   labels = c("Training set", "Test set"))
-
+#plotting predicted vs. calculated IE values for PFAS in calibration set
 IE_slope_cor = ggplot() +
   geom_point(data = datarbind_with_predicted %>%
                filter(instrument != "Orbitrap"),
@@ -282,7 +286,7 @@ IE_slope_cor = ggplot() +
 IE_slope_cor
 
 data_for_split = datarbind_with_predicted %>%
-  select(name, split_first, instrument, logIE, logIE_pred)%>%
+  select(name, split_first, instrument, logIE, logIE_predicted)%>%
   rename(Compound =name)
 
 data43254 = data_for_split %>% filter(instrument == "Orbitrap")
@@ -294,7 +298,7 @@ write.xlsx(data43254,"data/test_train_split.xlsx")
 
 IE_slope_cor_PFAS = ggplot(data = data43254 %>%
                         filter(instrument == "Orbitrap")) +
-  geom_point(mapping = aes(x = logIE,y = logIE_pred,
+  geom_point(mapping = aes(x = logIE,y = logIE_predicted,
                            color = Compound,
                            size = 0.25,
                            shape = Class)) +
@@ -307,23 +311,23 @@ IE_slope_cor_PFAS = ggplot(data = data43254 %>%
 IE_slope_cor_PFAS
 
 rmse((datarbind_with_predicted %>% filter(split_first == "Training set"))$logIE,
-     (datarbind_with_predicted %>% filter(split_first == "Training set"))$logIE_pred)
+     (datarbind_with_predicted %>% filter(split_first == "Training set"))$logIE_predicted)
 #0.46
 rmse((datarbind_with_predicted %>% filter(split_first == "Test set"))$logIE,
-     (datarbind_with_predicted %>% filter(split_first == "Test set"))$logIE_pred)
+     (datarbind_with_predicted %>% filter(split_first == "Test set"))$logIE_predicted)
 #0.74
 
 
 rmse((datarbind_with_predicted %>% filter(instrument == "Orbitrap" & split_first == "Training set"))$logIE,
-     (datarbind_with_predicted %>% filter(instrument == "Orbitrap" & split_first == "Training set"))$logIE_pred)
+     (datarbind_with_predicted %>% filter(instrument == "Orbitrap" & split_first == "Training set"))$logIE_predicted)
 #0.69
 rmse((datarbind_with_predicted %>% filter(instrument == "Orbitrap" & split_first == "Test set"))$logIE,
-     (datarbind_with_predicted %>% filter(instrument == "Orbitrap" & split_first == "Test set"))$logIE_pred)
+     (datarbind_with_predicted %>% filter(instrument == "Orbitrap" & split_first == "Test set"))$logIE_predicted)
 #0.50
 
 graph_retrainPFAS=ggplotly(IE_slope_cor_PFAS)
 graph_retrainPFAS
 
-htmlwidgets::saveWidget(plotly::as_widget(graph_retrainPFAS), "1stryPFAScal.html")
+htmlwidgets::saveWidget(plotly::as_widget(graph_retrainPFAS), "PFAS_IE_predvscalc.html")
 
 
